7d3a57a52b3fb52ac215c8c963832412b95db231
mitwa07 <mitwa5704@gmail.com>
2023-04-11 17:12:14 -0700

    Update lab2.ps1

--- powershell/lab2.ps1
+++ powershell/lab2.ps1
@@ -4,3 +4,12 @@
 write-output "Welcome to planet $env:computername Overlord $env:username"
 $now = get-date -format 'HH:MM tt on dddd'
 write-output "It is $now."
+
+# functionn get-mydisks
+
+
+function get-mydisks {
+    Get-PhysicalDisk | Select-Object Manufacturer, Model, SerialNumber, FirmwareRevision, Size | Format-Table
+}
+
+get-mydisks
