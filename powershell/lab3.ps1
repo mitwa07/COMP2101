bf43a7a1d84215cd8c4dd754a9fad87e0ba9a998
mitwa07 <122471634+mitwa07@users.noreply.github.com>
2023-04-11 20:07:35 -0400

    Delete lab3.ps1

--- powershell/lab3.ps1
+++
@@ -1,3 +0,0 @@
-gwmi -class win32_networkadapterconfiguration -filter ipenabled=true |
- where { $_.dnsdomain -ne $null -or $_.dnshostname -ne $null -or $_.dnsserversearchorder -ne $null } |
- select description, dnsserversearchorder, dnsdomain, dnshostname
